c86d358e79dbf20d848f9372fd87856f0cb76499
MANOJVARMA02 <89355838+MANOJVARMA02@users.noreply.github.com>
2022-08-10 11:11:31 +0530

    Update addData.py

--- addData.py
+++ addData.py
@@ -4,6 +4,14 @@ import random
 client = MongoClient()
 db=client.company
 emp=db.Employees
-for k in emp.find():
-    print(type(k))
+i=1
+while(i!=50):
+    dic={}
+    dic["_id"]=i
+    dic["name"]=input()
+    dic["age"]=random.randint(22,50)
+    dic["role"]=input()
+    dic["salary"]=random.randint(12000,50000)
+    i+=1
+    emp.insert_one(dic)
 
